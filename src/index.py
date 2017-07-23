ba08b38c8296e965593a0f5f79a0f3ae6f3bd49b
Terence Hegarty <tsh@terencehegarty.com>
2017-07-23 17:43:06 -0400

    reading files from s3

--- src/index.py
+++ src/index.py
@@ -3,30 +3,36 @@ import os
 import boto3
 import logging
 
+from boto3.session import Session
+
 # load yml file to dictionary
 s = os.path.dirname(__file__)
 f = os.path.join(s, 'conf.d/secret.yaml')
 creds = yaml.load(open(f))
 
-#creds = yaml.load(open(os.path.join(os.path.dirname(__file__),'/conf.d/secret.yaml')))
-#open("./secret.yaml")
-
-# access values from dictionary
-#aws_key = credentials['aws']['secret_key']
-
-#def _connect_s3(jw_config):
+#def _connect_s3():
 	#Creates a boto3 client from 
-        #@type jw_config: jwplayer.config.AppConfig object
-        #@param jw_config: AppConfig object with aws config variables
-        #@rtype: boto3.session.Session.client
-        #@return: boto3.session.Session.client, boto3 client
-	#logging.info('Attempting to connect to s3')
-    	
-#	session = boto3.session.Session(
- #       region_name=jw_config.aws.region,
-  #      aws_access_key_id=jw_config.aws.key,
-   #     aws_secret_access_key=jw_config.aws.secret
-    #)
-    #return session.client('s3', config=boto3.session.Config(signature_version='s3v4'))
-h = creds['credentials'] 
-print("secret_key:",h)
+   	#@type jw_config: jwplayer.config.AppConfig object
+  	#@param jw_config: AppConfig object with aws config variables
+   	#@rtype: boto3.session.Session.client
+   	#@return: boto3.session.Session.client, boto3 client
+
+   	#session = boto3.session.Session(
+   	#region_name = 'us_east_1',
+   	#aws_access_key_id = creds['aws']['AWSAccessKeyId'],
+   	#aws_secret_access_key = creds['aws']['AWSSecretKey']
+   	#)
+
+   	#return session.client('s3', config=boto3.session.Config(signature_version='s3v4'))
+
+
+session = Session(aws_access_key_id = creds['aws']['AWSAccessKeyId'],
+                  aws_secret_access_key = creds['aws']['AWSSecretKey'])
+s3 = session.resource('s3')
+your_bucket = s3.Bucket('tsh908')
+
+for s3_file in your_bucket.objects.all():
+    print(s3_file.key)
+
+
+
