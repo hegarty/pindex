1107f2c80474c967945e17c78896640f2b40f997
Terence Hegarty <tsh@terencehegarty.com>
2017-09-02 23:46:52 -0400

    Testing: facial detection, image labels w Rokognition

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM python:3.4-alpine
 COPY . /src
 WORKDIR /src
 RUN pip install -r requirements.txt
-#CMD ["python", "app.py"]
+#CMD ["python", "./src/index.py"]
 
--- src/index.py
+++ src/index.py
@@ -2,6 +2,7 @@ import yaml
 import os
 import boto3
 import logging
+import json
 
 from boto3.session import Session
 
@@ -29,10 +30,41 @@ creds = yaml.load(open(f))
 session = Session(aws_access_key_id = creds['aws']['AWSAccessKeyId'],
                   aws_secret_access_key = creds['aws']['AWSSecretKey'])
 s3 = session.resource('s3')
-your_bucket = s3.Bucket('tsh908')
+your_bucket = s3.Bucket('pindex908')
 
 for s3_file in your_bucket.objects.all():
     print(s3_file.key)
+
+#Labels
+if __name__ == "__main__":
+    fileName='surprised.jpg'
+    bucket='pindex908'
+
+    client=boto3.client('rekognition',
+  	aws_access_key_id = creds['aws']['AWSAccessKeyId'],
+    aws_secret_access_key = creds['aws']['AWSSecretKey'],
+	region_name='us-east-1')
+
+    response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':fileName}},MinConfidence=75)
+
+    print('Detected labels for ' + fileName)
+    for label in response['Labels']:
+        print (label['Name'] + ' : ' + str(label['Confidence']))
+
+#Detect Faces
+if __name__ == "__main__":
+    #fileName='input.jpg'
+    #bucket='bucket'
+    #client=boto3.client('rekognition')
+
+    response = client.detect_faces(Image={'S3Object':{'Bucket':bucket,'Name':fileName}},Attributes=['ALL'])
+
+    print('Detected faces for ' + fileName)
+    for faceDetail in response['FaceDetails']:
+        print('The detected face is between ' + str(faceDetail['AgeRange']['Low'])
+              + ' and ' + str(faceDetail['AgeRange']['High']) + ' years old')
+        print('Here are the other attributes:')
+        print(json.dumps(faceDetail, indent=4, sort_keys=True))
 
 
 
