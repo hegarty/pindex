99315a948700777689c06f51e0e2fccc1d08d165
Terence Hegarty <tsh@terencehegarty.com>
2017-10-25 23:03:16 -0400

    refactoring now that I understand namespacing and scope in Python. Also improved passing meta info to DDB

--- src/index.py
+++ src/index.py
@@ -28,7 +28,9 @@ rkg_client = boto3.client('rekognition',aws_access_key_id = aws_k,aws_secret_acc
 ddb_client = boto3.resource('dynamodb',aws_access_key_id = aws_k,aws_secret_access_key = aws_s,region_name='us-east-1')
 
 
-def ddb(resource_url, labels, age_range, emotions):
+def ddb(meta):
+	print('meta: ', json.dumps(meta))
+	'''
 	table = ddb_client.Table('r_test')
 	table.put_item(
 	Item={
@@ -37,141 +39,32 @@ def ddb(resource_url, labels, age_range, emotions):
 		'age_range': age_range,
 		'emotions': emotions
 		})
+	'''
 
 def get_filename(f):
 	n = f['Key'].rsplit('/', 1)
 	print ('FILE: '+s3_host+bucket+'/'+n[0],"---",repr(time.time()))
 	return n[0]
 
-def get_meta(f):
-	labels = []
-	filename = get_filename(f)
-	resource_url = s3_host+bucket+'/'+filename
-	rkg_res = rkg_client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':filename}},MinConfidence=98)
+def get_meta():
+	meta = {"resource_url":"none","labels":[],"emotions":[]}
 
-	for label in rkg_res['Labels']:
-		labels.append(label['Name'])
-
-	emotions = []
-	#filename = get_filename(f)
-	res = rkg_client.detect_faces(Image={'S3Object':{'Bucket':bucket,'Name':filename}},Attributes=['ALL'])
-	print("res['FaceDetails']: ",res['FaceDetails'])
-
-'''
-	for face_detail in res['FaceDetails']:
-		age_range = face_detail['AgeRange']
-		print('Emotions: ',face_detail['Emotions'])
-
-		for emt in face_detail['Emotions']:
-			emotions.append(emt['Type'])
-			print('Emotion Type: ',emt['Type'],filename)
-	print(resource_url,labels,age_range,emotions)
-	#ddb(resource_url,labels,"ok","nk")
-'''
-
-def get_labels(f):
-	filename = get_filename(f)
-	resource_url = s3_host+bucket+'/'+filename
-	rkg_res = rkg_client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':filename}},MinConfidence=98)
-	
-	for label in rkg_res['Labels']:
-		labels.append(label['Name'])
-
-	#ddb(resource_url,labels,"ok","nk")
-
-def get_emotions(f):
-	emotions = []
-	filename = get_filename(f)
-	res = rkg_client.detect_faces(Image={'S3Object':{'Bucket':bucket,'Name':filename}},Attributes=['ALL'])
-	for face_detail in res['FaceDetails']:
-		age_range = face_detail['AgeRange']
-		print('Emotions: ',face_detail['Emotions'])
-
-		for emt in face_detail['Emotions']:
-			emotions.append(emt['Type'])
-			print('Emotion Type: ',emt['Type'],filename)
-
-
-def init():
 	for f in s3_res['Contents']:
 		time.sleep(1)
-		get_meta(f)
-		#get_labels(f)
-		#get_emotions(f)
+		filename = get_filename(f)
+		meta['resource_url'] = s3_host+bucket+'/'+filename
+		rkg_res = rkg_client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':filename}},MinConfidence=98)
 
-init()
+		for label in rkg_res['Labels']:
+			meta['labels'].append(label['Name'])
 
-'''
-# Loop through each file and pull labels
-for f in s3_res['Contents']:
-	emotions = []
-	labels = []
-	time.sleep(1)
-	# Get the file name
-	n = f['Key'].rsplit('/', 1)
-	filename = n[0]
-	print ('FILE: '+s3_host+bucket+'/'+n[0],"---",repr(time.time()))
-	resource_url = s3_host+bucket+'/'+filename
-	rkg_res = rkg_client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':filename}},MinConfidence=98)
-	for label in rkg_res['Labels']:
-		labels.append(label['Name']) 
+		res = rkg_client.detect_faces(Image={'S3Object':{'Bucket':bucket,'Name':filename}},Attributes=['ALL'])
+		for face_detail in res['FaceDetails']:
+			meta['age_range'] = face_detail['AgeRange']
 
-#Detect Faces
-	face_res = rkg_client.detect_faces(Image={'S3Object':{'Bucket':bucket,'Name':filename}},Attributes=['ALL'])
-	for face_detail in face_res['FaceDetails']:
-		#meta['age_range']['low'] = face_detail['AgeRange']['Low']
-		#meta['age_range']['high'] = face_detail['AgeRange']['High']
-		age_range = face_detail['AgeRange']
-		print('Emotions: ',face_detail['Emotions'])
+			for emt in face_detail['Emotions']:
+				if not emt['Type'] in meta['emotions']:
+					meta['emotions'].append(emt['Type'])
 
-		for emt in face_detail['Emotions']:
-			et = emt['Type']+"_"+filename
-			emotions.append(et)
-			print('Emotion Type: ',emt['Type'],filename)
-
-#write to DDB
-	table = ddb_client.Table('r_test')
-	table.put_item(
-	Item={
-		'resource': resource_url,
-		'labels': labels,
-		'age_range': age_range,
-		'emotions': emotions
-		})
-'''
-	#print('Detected faces for ' + fn)
-	#for faceDetail in response['FaceDetails']:
-		#print('The detected face is between ' + str(faceDetail['AgeRange']['Low']) + ' and ' + str(faceDetail['AgeRange']['High']) + ' years old')
-		#print('Here are the other attributes:')
-        
-	#for key in response['FaceDetails']:
-		#print(key)
-		#if 'Emotions' in faceDetail: 
-			#for fd in faceDetail['Emotions']:
-				#print(json.dumps(fd['Type'], indent=4, sort_keys=True))
-#
-#dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
-#
-#table = dynamodb.Table('Movies')
-#
-#title = "The Big New Movie"
-#year = 2015
-#
-#response = table.put_item(
-#   Item={
-#        'year': year,
-#        'title': title,
-#        'info': {
-#            'plot':"Nothing happens at all.",
-#            'rating': decimal.Decimal(0)
-#        }
-#    }
-#)
-#
-#print("PutItem succeeded:")
-#
-
-
-#for s3_file in your_bucket.objects.all():
-#    for key in s3_file
-#   print(s3_file.key)
+		ddb(meta)
+get_meta()
